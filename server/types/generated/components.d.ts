b51cc13efff2cc61841bb320c8154a97637fe0f5
sohail <sohailcovermore@gmail.com>
2025-07-04 18:54:40 +0500

    Added components

---
+++ server/types/generated/components.d.ts
@@ -0,0 +1,64 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface BlocksHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+  };
+  attributes: {
+    cta: Schema.Attribute.Component<'elements.link', false>;
+    heading: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images'>;
+    logo: Schema.Attribute.Component<'elements.logo', false>;
+    theme: Schema.Attribute.Enumeration<['turquoise', 'orange']>;
+  };
+}
+
+export interface BlocksInfoBlock extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_info_blocks';
+  info: {
+    displayName: 'infoBlock';
+  };
+  attributes: {
+    cta: Schema.Attribute.Component<'elements.link', false>;
+    description: Schema.Attribute.RichText;
+    heading: Schema.Attribute.String;
+    infoImg: Schema.Attribute.Media<'images'>;
+    isReveresed: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    theme: Schema.Attribute.Enumeration<['turquoise', 'orange']>;
+  };
+}
+
+export interface ElementsLink extends Struct.ComponentSchema {
+  collectionName: 'components_elements_links';
+  info: {
+    displayName: 'link';
+  };
+  attributes: {
+    href: Schema.Attribute.String;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    text: Schema.Attribute.String;
+  };
+}
+
+export interface ElementsLogo extends Struct.ComponentSchema {
+  collectionName: 'components_elements_logos';
+  info: {
+    displayName: 'logo';
+  };
+  attributes: {
+    logo: Schema.Attribute.Media<'images', true>;
+    logoText: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'blocks.hero-section': BlocksHeroSection;
+      'blocks.info-block': BlocksInfoBlock;
+      'elements.link': ElementsLink;
+      'elements.logo': ElementsLogo;
+    }
+  }
+}
